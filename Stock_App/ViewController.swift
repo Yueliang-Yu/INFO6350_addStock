d0fafa512bf9f7318bfb7bd912cfadf62d4cb5f9
yuyueliang <2469536487@qq.com>
2023-10-21 15:39:53 -0700

    Initial Commit

---
+++ Stock_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock_App
+//
+//  Created by 余玥良 on 2023/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
