e659e309c8bf055cfec4f18869eb93e423a36a05
yuyueliang <2469536487@qq.com>
2023-10-21 21:38:51 -0700

    delete an error action in the inspector

---
+++ Stock_App/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Stock_App
+//
+//  Created by 余玥良 on 2023/10/21.
+//
+
+import Foundation
+
+let apiKey = "b139817f294b9a6f154c1aad6bbb7fe7"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
--- Stock_App/ViewController.swift
+++ Stock_App/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate, UITableViewDataSource {
 
+    let stockNames = ["MSFT", "TSLA", "META", "GOOG"]
+
+    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stockNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tblView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = stockNames[indexPath.row]
+        
+        return cell
+    }
 }
 
